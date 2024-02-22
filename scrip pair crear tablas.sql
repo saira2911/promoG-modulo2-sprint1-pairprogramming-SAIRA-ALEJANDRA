69579c4ed5410f31cb596df3a455c809bd90b930
saira2911 <152532982+saira2911@users.noreply.github.com>
2024-02-22 15:54:25 +0100

    correccion identaciones

--- scrip pair crear tablas.sql
+++ scrip pair crear tablas.sql
@@ -12,21 +12,21 @@ CREATE TABLE zapatillas(
 CREATE TABLE clientes(
 	id_clientes INT NOT NULL AUTO_INCREMENT,
 	nombre VARCHAR(45) NOT NULL,  
-    numero_telefono INT NOT NULL,
-    email VARCHAR(45) NOT NULL, 
-    direccion VARCHAR(45) NOT NULL, 
-    ciudad VARCHAR(45),
+    	numero_telefono INT NOT NULL,
+    	email VARCHAR(45) NOT NULL, 
+    	direccion VARCHAR(45) NOT NULL, 
+    	ciudad VARCHAR(45),
 	provincia VARCHAR(45) NOT NULL, 
-    país VARCHAR(45) NOT NULL,
-    codigo_postal VARCHAR(45) NOT NULL,
+    	país VARCHAR(45) NOT NULL,
+    	codigo_postal VARCHAR(45) NOT NULL,
 	PRIMARY KEY (id_clientes)
     );
 
 CREATE TABLE empleados(
 	id_empleado INT NOT NULL AUTO_INCREMENT,
 	nombre VARCHAR(45) NOT NULL,  
-    tienda VARCHAR(45) NOT NULL, 
-    salario INT, 
+    	tienda VARCHAR(45) NOT NULL, 
+    	salario INT, 
 	fecha_incorporation DATE NOT NULL, 
 	PRIMARY KEY (id_empleado)
     );
@@ -34,27 +34,27 @@ CREATE TABLE empleados(
 CREATE TABLE facturas(
 	id_factura INT NOT NULL AUTO_INCREMENT,
 	numero_factura VARCHAR(45) NOT NULL, 
-    fecha DATE NOT NULL, 
-    id_zapatilla INT NOT NULL,
-    id_clientes INT NOT NULL,
-    id_empleado INT NOT NULL,
+    	fecha DATE NOT NULL, 
+    	id_zapatilla INT NOT NULL,
+    	id_clientes INT NOT NULL,
+    	id_empleado INT NOT NULL,
     
 	PRIMARY KEY (id_factura),
     
-    CONSTRAINT fk_facturas_zapatillas
+    	CONSTRAINT fk_facturas_zapatillas
 		FOREIGN KEY (id_zapatilla)
-        REFERENCES zapatillas (id_zapatilla)
+        	REFERENCES zapatillas (id_zapatilla)
 		ON UPDATE CASCADE,                   
         
 	CONSTRAINT fk_facturas_clientes
 		FOREIGN KEY (id_clientes)
-        REFERENCES clientes (id_clientes)
-        ON UPDATE CASCADE,
+        	REFERENCES clientes (id_clientes)
+        	ON UPDATE CASCADE,
         
 	CONSTRAINT fk_facturas_empleados
 		FOREIGN KEY (id_empleado)
-        REFERENCES empleados (id_empleado)
-        ON UPDATE CASCADE
+        	REFERENCES empleados (id_empleado)
+        	ON UPDATE CASCADE
     );
 
 
